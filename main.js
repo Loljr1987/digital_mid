5130581959416eded224293e363467a0c1af9e3e
Pequeno <lucivaldojr@hotmail.com>
2023-12-26 16:06:15 -0300

    dando prosseguimento limpando nosso codigo

--- main.js
+++ main.js
@@ -4,23 +4,16 @@ function tocaSom (idElemnetoAudio) {
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0
-
-// enquanto = while
-while (contador < listaDeTeclas.length) {
+// enquanto = while // para = for
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
     const tecla = listaDeTeclas[contador];
-    const instrumento =  tecla.classList[1];   
-    // template string
+    const instrumento =  tecla.classList[1];
     const idAudio = `#som_${instrumento}`;
-    // console.log(idAudio);
-
+   
     tecla.onclick = function () {
         tocaSom(idAudio);
     }
 
-    contador =  contador + 1;
-
-    // console.log(contador);
-
  }
+ 
